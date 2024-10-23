fc78dc8045fb3e4f2553d83dfb85d0ae9e8727a1
Anna Komkova <ann.komkova98@gmail.com>
2024-10-24 01:20:03 +0300

    add grid, need to resize

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const imgContent = document.querySelectorAll('.img-content-hover')
+
+function showImgContent(e) {
+  for (var i = 0; i < imgContent.length; i++) {
+    let x = e.pageX
+    let y = e.pageY
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`
+  }
+}
+
+document.addEventListener('mousemove', showImgContent)
---
+++ webpack.common.js
@@ -0,0 +1,97 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'docs')
+    // clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env']]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2|eot)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+
+    // Landing page
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      filename: './index.html',
+      chunks: ['index']
+    })
+
+    // Partials
+    // new HtmlWebpackPartialsPlugin([
+    //   {
+    //     path: path.join(__dirname, './src/partials/analytics.html'),
+    //     location: 'analytics',
+    //     template_filename: '*',
+    //     priority: 'replace'
+    //   }
+    // ])
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
+  }
+}
